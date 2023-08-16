b8a0a353a713858ae2fb85f0aa4d2c7f8b97d95a
Jayveesuarez <141291190+Jayveesuarez@users.noreply.github.com>
2023-08-16 19:18:50 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function sendMail(){
+  var recipient="suarezjayvee";
+  var at = String.fromCharCode(64);
+  var dotcom="gmail.com";
+  var mail="mailto:";
+  window.open(mail+recipient+at+dotcom);
+}
